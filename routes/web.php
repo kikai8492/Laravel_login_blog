042ba0368befb4e5939d18f7e6790119d3e478a9
shuto kikai <yysskikai04@icloud.com>
2024-06-19 14:54:43 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\BlogsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::resource('/', BlogsController::class);
-// web.php
+
 Route::resource('/blogs', BlogsController::class);
+
+//ユーザー登録
+// Route::
