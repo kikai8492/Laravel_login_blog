6f3d6b90c458fe2d3b4b51acc9692fe10d2af8a1
shuto kikai <yysskikai04@icloud.com>
2024-06-22 06:41:14 +0900

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
 
         // ミドルウェアに対応したリダイレクト(後述)
         // 下記はredirect('/admin/blogs')に類似
-        return redirect()->intended('blogs/index');
+        return redirect()->intended('/blogs/index');
     }
 
     // ログイン情報が正しくない場合のみ実行される処理(return すると以降の処理は実行されないため)
